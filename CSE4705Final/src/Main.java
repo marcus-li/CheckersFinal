5a62a6df4f5909e56d4f7e0c92a4fb27d215c7d7
marcus-li <marcus-li@users.noreply.github.com>
2014-04-09 17:48:31 -0400

    Update Main.java

--- CSE4705Final/src/Main.java
+++ CSE4705Final/src/Main.java
@@ -1,5 +1,4 @@
 
-
 public class Main
 	{
 		public static void main(String[] args)
@@ -15,6 +14,7 @@ public class Main
 		public void print(String s)
 			{
 				System.out.println(s);
+				//is this working?
 			}
 	}
 
